89e19c5c5d71198f626a3fca8b073b1d75ce00f9
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-09-25 18:40:56 -0400

    got the title and content, but it is incorrect, but I have it working.

--- web_scrape.py
+++ web_scrape.py
@@ -4,13 +4,21 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import pymongo
+import time
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
+# launching chromedriver to automate the webscrape
 executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
 browser = Browser("chrome", **executable_path)
 
+# visiting the url and parsing the HTML
 news_url = "https://mars.nasa.gov/news/"
 browser.visit(news_url)
+time.sleep(5)
 html = browser.html
 news_soup = BeautifulSoup(html, "html.parser")
+
+# creating searches for the html to get the new title and article content.
+news_title = news_soup.body.find("div", class_= "content_title").get_text()
+news_content = news_soup.body.find("div", class_= "article_teaser_body").get_text()
