ba3d5356217725d78b3cd39c1cd7b6e666c6464e
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-09-27 20:33:40 -0400

    Scraped the image html, and working on the Mars Weather

--- web_scrape.py
+++ web_scrape.py
@@ -1,6 +1,6 @@
 # Dependencies
 from splinter import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import pandas as pd
 import requests
 import pymongo
@@ -17,8 +17,27 @@ news_url = "https://mars.nasa.gov/news/"
 browser.visit(news_url)
 time.sleep(5)
 html = browser.html
-news_soup = BeautifulSoup(html, "html.parser")
+news_soup = bs(html, "html.parser")
 
 # creating searches for the html to get the new title and article content.
-news_title = news_soup.body.find("div", class_= "content_title").get_text()
+news_title = news_soup.body.find("div", class_= "bottom_gradient").get_text()
 news_content = news_soup.body.find("div", class_= "article_teaser_body").get_text()
+
+print(news_title)
+print("---------------------------------------")
+print(news_content)
+
+jpl_nasa_url = "https://www.jpl.nasa.gov"
+images_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+browser.visit(images_url)
+html = browser.html
+images_soup = bs(html, "html.parser")
+
+relative_image_path = images_soup.find_all('img')[3]["src"]
+featured_image_url = jpl_nasa_url + relative_image_path
+print(featured_image_url)
+
+mars_weather_url = "https://twitter.com/marswxreport?lang=en"
+browser.visit(mars_weather_url)
+mars_weather_html = browser.html
+weather_soup = bs(mars_weather_html, "html.parser")
