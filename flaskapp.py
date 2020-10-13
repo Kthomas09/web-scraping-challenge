b9c9cfcae0b56a318006b5134f6f9b71dcf6216f
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-10-13 11:53:10 -0400

    Final Push

--- flaskapp.py
+++ flaskapp.py
@@ -1,17 +1,22 @@
+# Import Dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+# variable to run Flask
 app = Flask (__name__)
 
+# connecting Flask app to Mongodb
 app.config["MONGO_URI"] = "mongodb://localhost:27017/flaskapp"
 mongo = PyMongo(app)
 
+# home function for app to read into the Mars Dictionary in Data Base
 @app.route("/")
 def home():
     mars_dict = mongo.db.mars_dict.find_one()
     return render_template("index.html", mars_dict = mars_dict)
 
+# Function to initialize scrape
 @app.route("/scrape")
 def scrape():
     mars_dict = mongo.db.mars_dict
@@ -19,5 +24,6 @@ def scrape():
     mars_dict.update({}, mars_data, upsert=True)
     return redirect("/", code = 302)
 
+# Run flask app.
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -29,43 +29,39 @@ def scrape():
     news_title = news_soup.body.find("div", class_= "bottom_gradient").get_text()
     news_content = news_soup.body.find("div", class_= "article_teaser_body").get_text()
 
+    # scraping the featured image of the Martian surface
     jpl_nasa_url = "https://www.jpl.nasa.gov"
     images_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(images_url)
     html = browser.html
     images_soup = bs(html, "html.parser")
-
     relative_image_path = images_soup.find_all('img')[3]["src"]
     featured_image_url = jpl_nasa_url + relative_image_path
 
-
+    # scraping the martian facts
     mars_facts_url = "https://space-facts.com/mars/"
     mars_facts = pd.read_html(mars_facts_url)
-
-
     mars_facts_df = mars_facts[2]
     mars_facts_df.columns = ["Description", "Values"]
-
-
     mars_html = mars_facts_df.to_html()
     mars_html.replace("\n", " ")
-################################################
 
 # Mars Hemispheres
-    astro_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-    browser.visit(astro_url)
+    hemisphere_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemisphere_url)
     time.sleep(2)
     links = browser.find_by_css("a.product-item h3")
 # Retreive all items that contain Mars hemispheres information
     image_urls = []
     for i in range(len(links)):
-        hemi = {}
+        hemisphere = {}
         browser.find_by_css("a.product-item h3")[i].click()
-        hemi["img_url"] = browser.find_by_text("Sample")["href"]
-        hemi["title"] = browser.find_by_css("h2.title").text
-        image_urls.append(hemi)
+        hemisphere["img_url"] = browser.find_by_text("Sample")["href"]
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        image_urls.append(hemisphere)
         browser.back()
 
+    # creating a dictionary to store all the scraped data
     mars_dict = {
         "News_Title": news_title,
         "News_Article": news_content,
@@ -78,23 +74,5 @@ def scrape():
     
     return mars_dict
 
-    # hemisphere_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-    # browser.visit(hemisphere_url)
-    # hemisphere_html = browser.html
-    # hemisphere_soup = bs(hemisphere_html, "html.parser")
-
-    # hemisphere_items = hemisphere_soup.find_all("div", class_="item")
-
-    # image_urls =[]
-
-    # for i in hemisphere_items:
-    #     title = i.find("h3").text
-    #     img_url_1 = i.find("a", class_="itemLink product-item")["href"]
-    #     browser.visit(hemisphere_url+img_url_1)
-    #     img_html = browser.html
-    #     img_soup = bs(img_html, "html.parser")
-    #     img_url_2 = hemisphere_url + img_soup.find("img", class_="thumb")["src"]
-    #     image_urls.append({"title": title, "img_url":img_url_2})
-    
     
         
